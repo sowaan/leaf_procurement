264586941076f0b60eaf8c4ffb002a43f3a60289
Mubi <mubashirahmedabbasi64@gmail.com>
2025-06-16 11:07:04 +0500

    update status indicator colors in Day Setup list

--- leaf_procurement/leaf_procurement/doctype/day_setup/day_setup_list.js
+++ leaf_procurement/leaf_procurement/doctype/day_setup/day_setup_list.js
@@ -2,9 +2,9 @@ frappe.listview_settings['Day Setup'] = {
     add_fields: ['status'],
     get_indicator: function(doc) {
         if (doc.status === 'Opened') {
-            return [__('Opened'), 'red'];
+            return [__('Opened'), 'green'];
         } else if (doc.status === 'Closed') {
-            return [__('Closed'), 'green'];
+            return [__('Closed'), 'blue'];
         } else {
             return [__('Draft'), 'orange'];
         }
