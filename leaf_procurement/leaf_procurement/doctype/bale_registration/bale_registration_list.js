4587200571259ef5b9c1ce2fcc09627f3541e4d0
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2025-06-18 12:25:09 +0500

    update bale registration

--- leaf_procurement/leaf_procurement/doctype/bale_registration/bale_registration_list.js
+++ leaf_procurement/leaf_procurement/doctype/bale_registration/bale_registration_list.js
@@ -1,19 +1,21 @@
 frappe.listview_settings['Bale Registration'] = {
-    // get_indicator: function (doc) {
-    //       console.log(`Doc loaded: ${doc.name}, Status: ${doc.bale_status}`);   
-
-    //     if (doc.bale_status === 'In Registration') {
-    //         return [__('In Registration'), 'orange', 'bale_status,=,In Registration'];
-    //     } else if (doc.bale_status === 'In Buying') {
-    //         return [__('In Buying'), 'blue', 'bale_status,=,In Buying'];
-    //     } else if (doc.bale_status === 'In Weighment') {
-    //         return [__('In Weighment'), 'purple', 'bale_status,=,In Weighment'];
-    //     } else if (doc.bale_status === 'Completed') {
-    //         return [__('Completed'), 'green', 'bale_status,=,Completed'];
-    //     } else if (doc.bale_status === 'Cancelled') {
-    //         return [__('Cancelled'), 'red', 'bale_status,=,Cancelled'];
-    //     }
-    // },
+    add_fields: [
+        "bale_status",
+        "supplier_grower",
+    ],
+    get_indicator: function (doc) {
+        if (doc.bale_status === 'In Registration') {
+            return [__('• In Registration'), 'orange', 'bale_status,=,In Registration'];
+        } else if (doc.bale_status === 'In Buying') {
+            return [__('• In Buying'), 'blue', 'bale_status,=,In Buying'];
+        } else if (doc.bale_status === 'In Weighment') {
+            return [__('• In Weighment'), 'purple', 'bale_status,=,In Weighment'];
+        } else if (doc.bale_status === 'Completed') {
+            return [__('• Completed'), 'green', 'bale_status,=,Completed'];
+        } else if (doc.bale_status === 'Cancelled') {
+            return [__('• Cancelled'), 'red', 'bale_status,=,Cancelled'];
+        }
+    },
     onload: function(listview) {
         frappe.call({
             method: 'frappe.client.get_list',
