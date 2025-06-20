418b4b9bd5d3600dfef14dfc4730373cb0831781
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2025-06-20 10:41:56 +0500

    update leaf_procurement

--- leaf_procurement/leaf_procurement/doctype/leaf_procurement_sync_tool/leaf_procurement_sync_tool.js
+++ leaf_procurement/leaf_procurement/doctype/leaf_procurement_sync_tool/leaf_procurement_sync_tool.js
@@ -77,56 +77,70 @@ frappe.ui.form.on("Leaf Procurement Sync Tool", {
     sync_down_select_all(frm) {
         const is_checked = frm.doc.sync_down_select_all;
         frm.fields_dict.sync_down_select_all.df.label = is_checked ? 'Unselect All' : 'Select All';
+
         is_bulk_update = true;
+
+        // Update all checkboxes without triggering events
         sync_down_checkboxes.forEach(field => {
-            frm.set_value(field, is_checked);
+            frm.doc[field] = is_checked;
         });
+
+        // Refresh all checkbox fields at once
+        frm.refresh_fields(sync_down_checkboxes);
+
         is_bulk_update = false;
     },
+
 
     sync_up_select_all(frm) {
         const is_checked = frm.doc.sync_up_select_all;
         frm.fields_dict.sync_up_select_all.df.label = is_checked ? 'Unselect All' : 'Select All';
+
         is_bulk_update = true;
+
         sync_up_checkboxes.forEach(field => {
-            frm.set_value(field, is_checked);
+            frm.doc[field] = is_checked;
         });
+
+        frm.refresh_fields(sync_up_checkboxes);
+
         is_bulk_update = false;
     },
 
+
     // Individual sync_down checkbox handlers
-    ...Object.fromEntries(
-        sync_down_checkboxes.map(field => [
-            field,
-            function (frm) {
-                if (is_bulk_update) return;
+    // ...Object.fromEntries(
+    //     sync_down_checkboxes.map(field => [
+    //         field,
+    //         function (frm) {
+    //             if (is_bulk_update) return;
 
-                if (!frm.doc[field]) {
-                    frm.set_value('sync_down_select_all', 0);
-                    frm.fields_dict.sync_down_select_all.df.label = 'Select All';
-                } else if (sync_down_checkboxes.every(f => frm.doc[f])) {
-                    frm.set_value('sync_down_select_all', 1);
-                    frm.fields_dict.sync_down_select_all.df.label = 'Unselect All';
-                }
-            }
-        ])
-    ),
+    //             if (!frm.doc[field]) {
+    //                 frm.set_value('sync_down_select_all', 0);
+    //                 frm.fields_dict.sync_down_select_all.df.label = 'Select All';
+    //             } else if (sync_down_checkboxes.every(f => frm.doc[f])) {
+    //                 frm.set_value('sync_down_select_all', 1);
+    //                 frm.fields_dict.sync_down_select_all.df.label = 'Unselect All';
+    //             }
+    //         }
+    //     ])
+    // ),
 
-    // Individual sync_up checkbox handlers
-    ...Object.fromEntries(
-        sync_up_checkboxes.map(field => [
-            field,
-            function (frm) {
-                if (is_bulk_update) return;
+    // // Individual sync_up checkbox handlers
+    // ...Object.fromEntries(
+    //     sync_up_checkboxes.map(field => [
+    //         field,
+    //         function (frm) {
+    //             if (is_bulk_update) return;
 
-                if (!frm.doc[field]) {
-                    frm.set_value('sync_up_select_all', 0);
-                    frm.fields_dict.sync_up_select_all.df.label = 'Select All';
-                } else if (sync_up_checkboxes.every(f => frm.doc[f])) {
-                    frm.set_value('sync_up_select_all', 1);
-                    frm.fields_dict.sync_up_select_all.df.label = 'Unselect All';
-                }
-            }
-        ])
-    )
+    //             if (!frm.doc[field]) {
+    //                 frm.set_value('sync_up_select_all', 0);
+    //                 frm.fields_dict.sync_up_select_all.df.label = 'Select All';
+    //             } else if (sync_up_checkboxes.every(f => frm.doc[f])) {
+    //                 frm.set_value('sync_up_select_all', 1);
+    //                 frm.fields_dict.sync_up_select_all.df.label = 'Unselect All';
+    //             }
+    //         }
+    //     ])
+    // )
 });
