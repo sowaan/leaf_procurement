233d4b77339953d404432fe93f213a90bb940632
amirbalochsowaan <amir.baloch@ahwaal.com>
2025-06-23 14:20:55 +0500

    sync tool print data

--- leaf_procurement/leaf_procurement/doctype/leaf_procurement_sync_tool/leaf_procurement_sync_tool.py
+++ leaf_procurement/leaf_procurement/doctype/leaf_procurement_sync_tool/leaf_procurement_sync_tool.py
@@ -138,31 +138,33 @@ def sync_up():
 		print(f"Syncing up data to the server... {doctypes}")
 
 		for field, doctype in doctypes:
-			if doc.get(field):
-				print(f"Syncing from {doctype} to server...")
-				url = f'{settings.instance_url}/api/resource/{doctype}'
-				data = frappe.get_all(doctype, filters={"custom_is_sync": 0, 'docstatus': ['<', 2]}, pluck='name')
-				for name in data:
-					doc_data = frappe.get_doc(doctype, name)
-					if doctype == "Bale Registration":
-						doc_data.check_validations = 0
-						doc_data.day_setup = ""
+			if not doc.get(field): continue
 
-					doc_data = json.loads(doc_data.as_json())
-					doc_data["skip_autoname"] = True
-					doc_data["__islocal"] = 0
-					doc_data["servername"] = name
-					print(f"Syncing {doctype} record")
-					response = requests.post(url, headers=headers, json=doc_data)
-					print(doctype, name, 'custom_is_sync', "Want to check save record values")
-					if response.status_code == 200 or response.status_code == 201:
-						frappe.db.set_value(doctype, name, 'custom_is_sync', 1)
-						frappe.msgprint({
-							'title': _('Sync Successful'),
-							'message': _(f"Synced record {doc_data['name']} for {doctype}.")
-						})
-					else:
-						print(f"Failed to sync {doctype} {doc_data['name']}: {response.text}")
-						frappe.log_error(response.text, f"Failed to sync {doctype} {doc_data['name']}")
+			print(f"Syncing from {doctype} to server...")
+			url = f'{settings.instance_url}/api/resource/{doctype}'
+			data = frappe.get_all(doctype, filters={"custom_is_sync": 0, 'docstatus': ['<', 2]}, pluck='name')
+			print(f"\n\n--------Data Variable: {data} to server...\n\n")
+			for name in data:
+				doc_data = frappe.get_doc(doctype, name)
+				if doctype == "Bale Registration":
+					doc_data.check_validations = 0
+					doc_data.day_setup = ""
+
+				doc_data = json.loads(doc_data.as_json())
+				doc_data["skip_autoname"] = True
+				doc_data["__islocal"] = 0
+				doc_data["servername"] = name
+				print(f"Syncing {doctype} record")
+				response = requests.post(url, headers=headers, json=doc_data)
+				print(doctype, name, 'custom_is_sync', "Want to check save record values")
+				if response.status_code == 200 or response.status_code == 201:
+					frappe.db.set_value(doctype, name, 'custom_is_sync', 1)
+					frappe.msgprint({
+						'title': _('Sync Successful'),
+						'message': _(f"Synced record {doc_data['name']} for {doctype}.")
+					})
+				else:
+					print(f"Failed to sync {doctype} {doc_data['name']}: {response.text}")
+					frappe.log_error(response.text, f"Failed to sync {doctype} {doc_data['name']}")
 	except Exception as e:
 		frappe.log_error(str(e), "Sync Up Error",)
