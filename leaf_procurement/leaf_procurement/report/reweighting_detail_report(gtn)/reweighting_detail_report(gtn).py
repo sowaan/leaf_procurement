ee43a8000c81a1c80b6612af8da55f4b8cb799a1
umair2004 <mumairfqabbasi812@gmail.com>
2025-07-07 21:06:37 +0500

    updATE REports

---
+++ leaf_procurement/leaf_procurement/report/reweighting_detail_report(gtn)/reweighting_detail_report(gtn).py
@@ -0,0 +1,66 @@
+# Copyright (c) 2025, Sowaan and contributors
+# For license information, please see license.txt
+
+import frappe
+
+def execute(filters=None):
+	columns = get_columns()
+	data = get_data(filters)
+	return columns, data
+
+
+def get_filters(filters):
+	conditions = ""
+	if not filters:
+		filters = frappe._dict()
+	if filters.get("from_date") and filters.get("to_date"):
+		conditions += " AND gtn.date BETWEEN %(from_date)s AND %(to_date)s"
+	elif filters.get("from_date"):
+		conditions += " AND gtn.date >= %(from_date)s"
+	elif filters.get("to_date"):
+		conditions += " AND gtn.date <= %(to_date)s"
+	if filters.get("depot"):
+		conditions += " AND gtn.location_warehouse = %(depot)s"
+	return conditions
+
+def get_columns():
+	return [
+		{"label": "BALE ID", "fieldname": "bale_barcode", "fieldtype": "Data", "width": 120},
+		{"label": "Warehouse", "fieldname": "location_warehouse", "fieldtype": "Link", "options": "Warehouse", "width": 140},
+		{"label": "Date", "fieldname": "date", "fieldtype": "Date", "width": 110},
+		{"label": "TSA#", "fieldname": "tsa", "fieldtype": "Data", "width": 140},
+		{"label": "GTN", "fieldname": "gtn", "fieldtype": "Link", "options": "Goods Transfer Note", "width": 180},
+		{"label": "Truck Number", "fieldname": "truck_number", "fieldtype": "Data", "width": 130},
+		{"label": "Advance Weight", "fieldname": "advance_weight", "fieldtype": "Float", "width": 130},
+		{"label": "Re-Weight", "fieldname": "re_weight", "fieldtype": "Float", "width": 130},
+		{"label": "Weight Difference", "fieldname": "weight_difference", "fieldtype": "Float", "width": 150},
+		{"label": "Bales", "fieldname": "bales", "fieldtype": "Int", "width": 80},
+	]
+
+
+def get_data(filters):
+	conditions = get_filters(filters)
+
+	data = frappe.db.sql(f"""
+		SELECT
+			gtni.bale_barcode AS bale_barcode,
+			gtn.location_warehouse AS location_warehouse,
+			gtn.date AS date,
+			gtn.tsa_number AS tsa,
+			gtn.name AS gtn,
+			bag.truck_number AS truck_number,
+			gtni.weight AS advance_weight,
+			IFNULL(bad.weight, 0) AS re_weight,
+			IFNULL(bad.weight, 0) - gtni.weight AS weight_difference,
+			1 AS bales
+		FROM `tabGoods Transfer Note Items` gtni
+		LEFT JOIN `tabGoods Transfer Note` gtn ON gtni.parent = gtn.name
+		LEFT JOIN `tabBale Audit Detail` bad ON bad.bale_barcode = gtni.bale_barcode
+		LEFT JOIN `tabBale Audit` ba ON bad.parent = ba.name
+		LEFT JOIN `tabBale Audit GTN` bag ON bag.parent = ba.name
+		WHERE gtn.docstatus = 1
+		{conditions}
+		ORDER BY gtni.bale_barcode
+	""", filters, as_dict=True)
+
+	return data
---
+++ leaf_procurement/leaf_procurement/report/warehouse_wise_bale_audit_report/warehouse_wise_bale_audit_report.js
@@ -0,0 +1,37 @@
+// Copyright (c) 2025, Sowaan and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["WAREHOUSE WISE BALE AUDIT REPORT"] = {
+	"filters": [
+		{
+			"fieldname": "from_date",
+			"label": "From Date",
+			"fieldtype": "Date",
+			"reqd": 1
+		},
+		{
+			"fieldname": "to_date",
+			"label": "To Date",
+			"fieldtype": "Date",
+			"reqd": 1
+		},
+		{
+			"fieldname": "grade",
+			"label": "Item Grade",
+			"fieldtype": "Link",
+			"options": "Item Grade"
+		},
+		{
+			"fieldname": "sub_grade",
+			"label": "Item Sub Grade",
+			"fieldtype": "Link",
+			"options": "Item Sub Grade"
+		},
+		{
+			"fieldname": "warehouse",
+			"label": "Warehouse",
+			"fieldtype": "Link",
+			"options": "Warehouse"
+		}
+	]
+};
