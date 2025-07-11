1b93f3dc8d37286465786baefc2f19c4057f8675
amirbalochsowaan <amir.baloch@ahwaal.com>
2025-07-11 21:10:36 +0500

    cost center also for rejected items

--- leaf_procurement/leaf_procurement/utils/sync_up.py
+++ leaf_procurement/leaf_procurement/utils/sync_up.py
@@ -100,6 +100,10 @@ def sync_single_record(doctype: str, name: str, url: str, headers: dict):
             for item in doc.items:
                 item.cost_center = "Main - SG"
 
+            if hasattr(doc, "custom_rejected_items"):
+                for item in doc.custom_rejected_items:
+                    if hasattr(item, "cost_center"):
+                        item.cost_center = "Main - SG"
 
         if doctype == "Bale Audit":
             # Check if Audit Day is closed
