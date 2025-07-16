de1d5035c133ccfa06660214982c4aee1c91e5aa
amirbalochsowaan <amir.baloch@ahwaal.com>
2025-07-16 22:30:26 +0500

    sync up message variable

--- leaf_procurement/leaf_procurement/utils/sync_up.py
+++ leaf_procurement/leaf_procurement/utils/sync_up.py
@@ -104,7 +104,8 @@ def sync_single_record(doctype: str, name: str, url: str, headers: dict):
         #         for item in doc.custom_rejected_items:
         #             if hasattr(item, "cost_center"):
         #                 item.cost_center = "Main - SG"
-
+        message = ""
+        
         if doctype == "Bale Audit":
             # Check if Audit Day is closed
             audit_day = frappe.db.get_value(
