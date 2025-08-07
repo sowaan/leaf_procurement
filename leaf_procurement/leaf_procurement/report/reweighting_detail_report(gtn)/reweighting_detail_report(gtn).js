aaf860b94c27a6c8cb2de182d2fa6c5eba2735f9
Mubi <mubashirahmedabbasi64@gmail.com>
2025-08-07 15:27:05 +0500

    Refactor Reweighting Detail Report to implement date range filters and enhance query conditions

--- leaf_procurement/leaf_procurement/report/reweighting_detail_report(gtn)/reweighting_detail_report(gtn).js
+++ leaf_procurement/leaf_procurement/report/reweighting_detail_report(gtn)/reweighting_detail_report(gtn).js
@@ -4,26 +4,51 @@
 frappe.query_reports["Reweighting Detail Report(GTN)"] = {
 	"filters": [
 		{
-			fieldname: "date",
+			fieldname: "from_date",
 			fieldtype: "Date",
-			label: "Date",
+			label: __("From Date"),
 			default: "Today",
 			mandatory: 1
 		},
 		{
+			fieldname: "to_date",
+            fieldtype: "Date",
+            label: __("To Date"),
+            default: "Today",
+            mandatory: 1
+        },
+		{
+			fieldname: "gtn",
+            fieldtype: "Link",
+            label: __("GTN"),
+            options: "Goods Transfer Note",
+		},
+		{
 			fieldname: "depot",
-			fieldtype: "Link",
-			label: __("Warehouse"),
-			options: "Warehouse"
-			// mandatory: 1
+			fieldtype: "MultiSelectList",
+			label: __("Depot"),
+			options: "Warehouse",
+			get_data: function (txt) {
+				return frappe.db.get_link_options("Warehouse", txt, {
+					is_group: 0,
+					custom_is_depot: 1,
+				});
+			},
 			
 		},
-        // {
-        //     default: "Today",
-        //     fieldname: "to_date",
-        //     fieldtype: "Date",
-        //     label: "To Date",
-        //     mandatory: 1
-        // }
+		{
+			fieldname: "warehouse",
+			fieldtype: "MultiSelectList",
+			label: __("Warehouse"),
+			options: "Warehouse",
+			get_data: function (txt) {
+				return frappe.db.get_link_options("Warehouse", txt, {
+					is_group: 0,
+					custom_is_depot: 0,
+				});
+			},
+			
+		},
+        
 	]
 };
--- leaf_procurement/leaf_procurement/report/reweighting_detail_report(gtn)/reweighting_detail_report(gtn).py
+++ leaf_procurement/leaf_procurement/report/reweighting_detail_report(gtn)/reweighting_detail_report(gtn).py
@@ -11,19 +11,46 @@ def execute(filters=None):
 
 def get_filters(filters):
 	conditions = ""
+	params = {}
+
 	if not filters:
 		filters = frappe._dict()
-	if filters.get("date"):
-		conditions += " AND gtn.date = %(date)s"
-	if filters.get("depot"):
-		conditions += " AND gtn.location_warehouse = %(depot)s"
-	return conditions
+
+	# Date filter
+	if filters.get("from_date") and filters.get("to_date"):
+		conditions += " AND gtn.date BETWEEN %(from_date)s AND %(to_date)s"
+		params["from_date"] = filters["from_date"]
+		params["to_date"] = filters["to_date"]
+
+	# GTN filter
+	if filters.get("gtn"):
+		conditions += " AND gtn.name = %(gtn)s"
+		params["gtn"] = filters["gtn"]
+
+	# Depot MultiSelectList
+	depot_list = filters.get("depot") or []
+	if depot_list:
+		placeholders = ', '.join([f"%({f'depot_{i}'})s" for i in range(len(depot_list))])
+		conditions += f" AND gtn.location_warehouse IN ({placeholders})"
+		for i, val in enumerate(depot_list):
+			params[f"depot_{i}"] = val
+
+	# Warehouse MultiSelectList
+	warehouse_list = filters.get("warehouse") or []
+	if warehouse_list:
+		placeholders = ', '.join([f"%({f'warehouse_{i}'})s" for i in range(len(warehouse_list))])
+		conditions += f" AND gtn.receiving_location IN ({placeholders})"
+		for i, val in enumerate(warehouse_list):
+			params[f"warehouse_{i}"] = val
+
+	return conditions, params
+
 
 def get_columns():
 	return [
 		{"label": "BALE ID", "fieldname": "bale_barcode", "fieldtype": "Data", "width": 120},
-		{"label": "Warehouse", "fieldname": "location_warehouse", "fieldtype": "Link", "options": "Warehouse", "width": 140},
-		{"label": "Location Warehouse", "fieldname": "l_warehouse", "fieldtype": "Link", "options": "Warehouse", "width": 140},
+		{"label": "Depot", "fieldname": "location_warehouse", "fieldtype": "Link", "options": "Warehouse", "width": 140},
+		{"label": "Warehouse", "fieldname": "l_warehouse", "fieldtype": "Link", "options": "Warehouse", "width": 140},
 		{"label": "Date", "fieldname": "date", "fieldtype": "Date", "width": 110},
 		{"label": "TSA#", "fieldname": "tsa", "fieldtype": "Data", "width": 140},
 		{"label": "GTN", "fieldname": "gtn", "fieldtype": "Link", "options": "Goods Transfer Note", "width": 180},
@@ -31,49 +58,33 @@ def get_columns():
 		{"label": "Advance Weight", "fieldname": "advance_weight", "fieldtype": "Float", "width": 130},
 		{"label": "Re-Weight", "fieldname": "re_weight", "fieldtype": "Float", "width": 130},
 		{"label": "Weight Difference", "fieldname": "weight_difference", "fieldtype": "Float", "width": 150},
+		{"label": "Remarks", "fieldname": "remarks", "fieldtype": "Data", "width": 200},
 		{"label": "Bales", "fieldname": "bales", "fieldtype": "Int", "width": 80},
 	]
 
 
 def get_data(filters):
-	conditions = get_filters(filters)
-# SELECT
-#     bad.bale_barcode AS bale_barcode,
-#     ba.location_warehouse AS location_warehouse,
-#     ba.location_warehouse AS l_warehouse,  -- redundant, but kept as per your original query
-#     ba.date AS date,
-#     bad.tsa_number AS tsa,
-#     bad.gtn_number AS gtn,
-#     bad.truck_number AS truck_number,
-#     bad.advance_weight AS advance_weight,
-#     IFNULL(bad.weight, 0) AS re_weight,
-#     IFNULL(bad.weight, 0) - bad.advance_weight AS weight_difference,
-#     1 AS bales
-# FROM `tabBale Audit Detail` AS bad
-# LEFT JOIN `tabBale Audit` AS ba ON bad.parent = ba.name
-# WHERE ba.docstatus = 1
+	conditions, sql_params = get_filters(filters)
 
 	data = frappe.db.sql(f"""
 		SELECT
 			gtni.bale_barcode AS bale_barcode,
 			gtn.location_warehouse AS location_warehouse,
-			ba.location_warehouse AS l_warehouse,
+			gtn.receiving_location AS l_warehouse,
 			gtn.date AS date,
 			gtn.tsa_number AS tsa,
 			gtn.name AS gtn,
-			bag.truck_number AS truck_number,
+			gtn.vehicle_number AS truck_number,
 			gtni.weight AS advance_weight,
-			IFNULL(bad.weight, 0) AS re_weight,
-			IFNULL(bad.weight, 0) - gtni.weight AS weight_difference,
+			0 AS re_weight,
+			(0 - gtni.weight) AS weight_difference,
+			'' AS Remarks,
 			1 AS bales
 		FROM `tabGoods Transfer Note Items` gtni
 		LEFT JOIN `tabGoods Transfer Note` gtn ON gtni.parent = gtn.name
-		LEFT JOIN `tabBale Audit Detail` bad ON bad.bale_barcode = gtni.bale_barcode
-		LEFT JOIN `tabBale Audit` ba ON bad.parent = ba.name
-		LEFT JOIN `tabBale Audit GTN` bag ON bag.parent = ba.name
 		WHERE gtn.docstatus = 1
 		{conditions}
 		ORDER BY gtni.bale_barcode
-	""", filters, as_dict=True)
+	""", sql_params, as_dict=True)
 
 	return data
