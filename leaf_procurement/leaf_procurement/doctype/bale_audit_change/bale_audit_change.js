fd925f25ed4b35d84e9f22177fb17ea94729959f
amirbalochsowaan <amir.baloch@ahwaal.com>
2025-09-04 15:21:12 +0500

    Bale Audit Change Utility Final

--- leaf_procurement/leaf_procurement/doctype/bale_audit_change/bale_audit_change.js
+++ leaf_procurement/leaf_procurement/doctype/bale_audit_change/bale_audit_change.js
@@ -3,14 +3,20 @@
 
 frappe.ui.form.on("Bale Audit Change", {
 	refresh(frm) {
-
+        $(".layout-side-section").hide();   // hide sidebar
+        $(".sidebar-toggle-btn").hide();    // hide toggle
 	},
+    bale_barcode(frm) {
+        frm.set_value("new_bale_barcode", frm.doc.bale_barcode);
+    },
     change_weight(frm) {
         if (frm.is_new()) {
             frappe.msgprint("⚠️ Please save the record first before changing weight.");
             return;
         }
-
+        if (!frm.doc.bale_barcode || !frm.doc.new_bale_barcode || !frm.doc.new_weight) {
+            frappe.throw("❌ Bale Barcode, New Bale Barcode and New Weight are required.");
+        }
         let values = frm.doc;
 
         frappe.call({
@@ -18,6 +24,7 @@ frappe.ui.form.on("Bale Audit Change", {
             args: {
                 parent: frm.doc.name,              // valid docname after save
                 barcode: values.bale_barcode,
+                new_barcode: values.new_bale_barcode,
                 new_weight: values.new_weight
             },
             callback: function (r) {
