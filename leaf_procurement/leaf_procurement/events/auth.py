368b154e70345e090a3330e76b7c21a5129453e7
Mubi <mubashirahmedabbasi64@gmail.com>
2025-09-12 15:33:45 +0500

    Escape Administrator for API access in successful_login function

--- leaf_procurement/leaf_procurement/events/auth.py
+++ leaf_procurement/leaf_procurement/events/auth.py
@@ -7,7 +7,7 @@ def successful_login(login_manager):
     api_key = frappe.db.get_value("User", user, "api_key")
     api_secret = frappe.db.get_value("User", user, "api_secret")
 
-    # If user has both API Key and Secret, block login
-    if api_key and api_secret:
+    # If user has both API Key and Secret, block login, except for Administrator
+    if api_key and api_secret and not user == "Administrator":
         frappe.throw(_("This user is restricted to API access only."), frappe.AuthenticationError)
         
