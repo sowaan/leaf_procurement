6c35130662188a985f4f4a7b28930c41033f0c33
tafheem-alam <tafheem.alam@sowaan.com>
2026-02-04 16:42:58 +0500

    Created Process Order Listing Report

---
+++ leaf_procurement/leaf_manufacturing/report/process_order_listing_report/process_order_listing_report.js
@@ -0,0 +1,22 @@
+frappe.query_reports["Process Order Listing Report"] = {
+	filters: [
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1)
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today()
+		},
+		{
+			fieldname: "location",
+			label: __("Location"),
+			fieldtype: "Link",
+			options: "Warehouse"
+		}
+	]
+};
